2d3f328d38a349de5d7f4503e101bf89a8c0a816
Liwott <david.defilippi@hotmail.be>
2021-04-26 12:28:06 +0200

    remove locale from classes. fix noData

--- classes.py
+++ classes.py
@@ -11,39 +11,33 @@ class TextArg(object):
         return str(self.number)
 
 class DataModel(object):
-    def __init__(self,cost):
+    def __init__(self,cost,text:str,formatters:dict={}):
         self.cost = cost
-        self.text={}
-        self.formatters={}
-
-    def add_text(self,locale:str,text:str,formatters:list=[]):
-        self.text[locale]=text+' '
-        self.formatters[locale]=formatters
+        self.text=text+' '
+        self.formatters=formatters
 
 class Data(object):
     def __init__(self,dataModel,*args):
         self.dataModel=dataModel
         self.args=args
         self.cost=dataModel.cost(*args)
-        self.create_text()
-
-    def create_text(self):
         self.text={}
-        for locale in self.dataModel.text:
-            textArgs=[""]
-            textModel=self.dataModel.text[locale]
-            formatters=self.dataModel.formatters[locale]
-            for arg in self.args:
-                textArg=TextArg(arg)
-                for key in formatters:
-                    setattr(textArg,key,formatters[key](arg))
-                textArgs.append(textArg)
-            self.text[locale]=textModel.format(*textArgs)
+        textArgs=[""]
+        textModel=self.dataModel.text
+        formatters=self.dataModel.formatters
+        for arg in self.args:
+            textArg=TextArg(arg)
+            for key in formatters:
+                setattr(textArg,key,formatters[key](arg))
+            textArgs.append(textArg)
+        self.text=textModel.format(*textArgs)
 
 class NoData(object):
     def __init__(self):
-        self.cost=None
         self.args=[None]
+        self.cost=0
+        self.text=""
+        self.formatters={}
 noData=NoData()
 
 class EffectModel(object):
@@ -59,22 +53,17 @@ class Effect(object):
         self.args=args
         self.targetting=targetting
         self.cost=dataModel.cost(targetting.cost,*args)
-        self.create_text()
-
-    def create_text(self):
-        self.text={}
-        for locale in self.dataModel.text:
-            textModel=self.dataModel.text[locale]
-            formatters=self.dataModel.formatters[locale]
-            textArgs=[]
-            # the last argument of the targetting is onsidered to be the number of targets
-            args=[self.targetting.args[-1]]+list(self.args)
-            for arg in args:
-                textArg=TextArg(arg)
-                for key in formatters:
-                    setattr(textArg,key,formatters[key](arg))
-                textArgs.append(textArg)
-            self.text[locale]=textModel.format(*textArgs)
+        textModel=self.dataModel.text
+        formatters=self.dataModel.formatters
+        textArgs=[]
+        # the last argument of the targetting is considered to be the number of targets
+        args=[self.targetting.args[-1]]+list(self.args)
+        for arg in args:
+            textArg=TextArg(arg)
+            for key in formatters:
+                setattr(textArg,key,formatters[key](arg))
+            textArgs.append(textArg)
+        self.text=textModel.format(*textArgs)
 
 class Ability(object):
     def __init__(self, actCondition=noData,actCost=noData,targetting=noData, *effectModels):
@@ -91,13 +80,10 @@ class Ability(object):
         cost-=actCondition.cost
         cost-=actCost.cost
         self.cost=cost
-        self.text={}
-
-    def create_text(self,locale):
         text=""
-        text+=self.actCondition.text[locale]
-        text+=self.actCost.text[locale]
-        text+=self.targetting.text[locale]
+        text+=self.actCondition.text
+        text+=self.actCost.text
+        text+=self.targetting.text
         for effect in self.effects:
-            text+=effect.text[locale]
-        self.text[locale]=text
+            text+=effect.text
+        self.text=text
--- example.py
+++ example.py
@@ -1,57 +1,47 @@
 import classes as c
 
-# def cost0(x,y,z):
-#     return x*y*z
-# type0=DataModel(cost0)
-# type0.add_text("EN","lmao{1.s} lmao{2.s} lmao{3.s}")
-# type0.add_text("FR","mdr{1.s} mdr{2.s} mdr{3.s}")
-# data0=Data(type0,2,1,3)
-# print(data0.cost)
-# print(data0.text)
-
 print("Spell Card : Black Death")
-print("Activation Condition")
-exActConditionModel=c.DataModel(lambda x : x)
+# print("Activation Condition")
 def format_are(n):
     if n==1:
         return "is"
     else:
         return "are"
-exActConditionModel.add_text("EN","""
-Activate when at least {1} of your cards in play (excluding this card) {1.are} destroyed.""",{"are":format_are})
+exActConditionModel=c.DataModel(lambda x : x,"Activate when at least {1} of your cards in play (excluding this card) {1.are} destroyed.",{"are":format_are})
 exActCondition=c.Data(exActConditionModel,1)
-print("cost : ",exActCondition.cost)
-print("EN : ",exActCondition.text["EN"])
+# print("cost : ",exActCondition.cost)
+# print("EN : ",exActCondition.text)
 
-print("Activation Cost")
-exActCostModel=c.DataModel(lambda x: 6*x)
-exActCostModel.add_text("EN","Sacrifice {1} card{1.s} in play chosen by an opponent of your choice.")
+# print("Activation Cost")
+exActCostModel=c.DataModel(lambda x: 6*x,"Sacrifice {1} card{1.s} in play chosen by an opponent of your choice.")
 exActCost=c.Data(exActCostModel,2)
-print("cost : ",exActCost.cost)
-print("EN : ",exActCost.text["EN"])
+# print("cost : ",exActCost.cost)
+# print("EN : ",exActCost.text)
 
-print("Targetting")
-exTargettingModel=c.DataModel(lambda x: x/2)
-exTargettingModel.add_text("EN","Target up to {1} exhausted card{1.s} in play.")
+# print("Targetting")
+exTargettingModel=c.DataModel(lambda x: x/2,"Target up to {1} exhausted card{1.s} in play.")
 exTargetting=c.Data(exTargettingModel,4)
-print("cost : ",exTargetting.cost)
-print("EN : ",exTargetting.text["EN"])
+# print("cost : ",exTargetting.cost)
+# print("EN : ",exTargetting.text)
 
-print("Effect")
-exEffectDataModel=c.DataModel(lambda x: 8*x)
+# print("Effect")
 def format_alltargets(n):
     if n==1:
         return "the target"
     else:
         return "all targets"
-exEffectDataModel.add_text("EN","Destroy {0.alltargets}.",{"alltargets":format_alltargets})
+exEffectDataModel=c.DataModel(lambda x: 8*x,"Destroy {0.alltargets}.",{"alltargets":format_alltargets})
 exEffectModel=c.EffectModel(exEffectDataModel)
 exEffect=c.Effect(exEffectModel,exTargetting)
-print("cost : ",exEffect.cost)
-print("EN : ",exEffect.text["EN"])
+# print("cost : ",exEffect.cost)
+# print("EN : ",exEffect.text)
 
-print("Ability")
+# print("Ability")
 exAbility=c.Ability(exActCondition,exActCost,exTargetting,exEffectModel)
-exAbility.create_text("EN")
 print("cost : ",exAbility.cost)
-print("EN : ",exAbility.text["EN"])
+print(exAbility.text)
+
+print("Spell Card : Free Black Death")
+exAbility=c.Ability(c.noData,c.noData,exTargetting,exEffectModel)
+print("cost : ",exAbility.cost)
+print(exAbility.text)
