dc49f1c81d1140cfd99d8555abf4fcb40c25b768
Liwott <david.defilippi@hotmail.be>
2021-12-24 13:40:42 +0100

    first test. failed : antibiotic, animal shelter

--- example.py
+++ example.py
@@ -1,9 +1,18 @@
 import classes
+import json
 
 dbEN=classes.DataBase("data/DataMapEN.json")
-print("Anubis (EN)")
-print(dbEN.refBareCard('C.5.25-6.3/18.1-0-5.2-32.2/0-0-3.1.2-10'))
-print("")
+file=open("sets/cards.json")
+set=json.load(file)
+file.close()
+for name,ref in set.items():
+    print(name+ "(EN)")
+    print(dbEN.refBareCard(ref))
+    print("")
+
+# print("Anubis (EN)")
+# print(dbEN.refBareCard('C.5.25-6.3/18.1-0-5.2-32.2/0-0-3.1.2-10'))
+# print("")
 
 dbFR=classes.DataBase("data/DataMapFR.json")
 print("Anubis (FR)")
