e15e0b29bc821ca78250c2d726cff42a681d3a55
Liwott <david.defilippi@hotmail.be>
2021-12-26 13:16:31 +0100

    structure

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
-import classes
+import ccftk
 
-dbEN=classes.DataBase("data/DataMapEN.json")
+dbEN=ccftk.DataBase("data/DataMapEN.json")
 print("Anubis (EN)")
 print(dbEN.refBareCard('C.5.25-6.3/18.1-0-5.2-32.2/0-0-3.1.2-10'))
 print("")
 
-dbFR=classes.DataBase("data/DataMapFR.json")
+dbFR=ccftk.DataBase("data/DataMapFR.json")
 print("Anubis (FR)")
 print(dbFR.refBareCard('C.5.25-6.3/18.1-0-5.2-32.2/0-0-3.1.2-10'))
 print("")
