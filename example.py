58340266274768a2a686265b8bdd6f34d43fe70f
Liwott <david.defilippi@hotmail.be>
2021-12-28 21:44:05 +0100

    sets

--- example.py
+++ example.py
@@ -15,6 +15,33 @@ for name,ref in set.items():
     print(dbEN.refBareCard(ref))
     print("")
 
+# file=open("sets/set1.json")
+# set=json.load(file)
+# file.close()
+# for name,ref in set.items():
+#     print(name+ " (EN)")
+#     print(ref)
+#     print(dbEN.refBareCard(ref))
+#     print("")
+# 
+# file=open("sets/set2.json")
+# set=json.load(file)
+# file.close()
+# for name,ref in set.items():
+#     print(name+ " (EN)")
+#     print(ref)
+#     print(dbEN.refBareCard(ref))
+#     print("")
+# 
+# file=open("sets/set3.json")
+# set=json.load(file)
+# file.close()
+# for name,ref in set.items():
+#     print(name+ " (EN)")
+#     print(ref)
+#     print(dbEN.refBareCard(ref))
+#     print("")
+
 # print("Anubis (EN)")
 # print(dbEN.refBareCard('C.5.25-6.3/18.1-0-5.2-32.2/0-0-3.1.2-10'))
 # print("")
